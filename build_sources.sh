8c77d712df4a7e926c5ad6985ba83cfe07cb374d
midd1eton <Tchaikovsky1$>
2022-03-24 13:07:34 +0000

    Updated Strcture and started with 0.1a tracking implementation.

--- build_sources.sh
+++ build_sources.sh
@@ -1,13 +1,20 @@
 echo "Please place all sources into the sources directory. "
-root_dir=$(pwd)
+root_dir=$(pwd)/deps
 noCores=$(nproc)
 
 ## opencv extract and build
+mkdir -p $root_dir/builds/opencv;
+mkdir -p $root_dir/sources/opencv;
 
-cd $root_dir/sources/opencv || exit
+
+## go to downloaded opencv install
 echo $(pwd)
+echo $root_dir
 
 OPENCV_CMAKE_OPTIONS=(-DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON);
-cmake ../opencv-4.5.5/ "${OPENCV_CMAKE_OPTIONS[@]}" -DCMAKE_INSTALL_PREFIX="$root_dir"/builds/opencv
+cmake -S $root_dir/downloads/opencv -B $root_dir/sources/opencv "${OPENCV_CMAKE_OPTIONS[@]}" -DCMAKE_INSTALL_PREFIX="$root_dir"/builds/opencv;
+## goto source dir
+cd $root_dir/sources/opencv || exit
+## build
 make -j $noCores
 make install
--- modules/tracking/main.cpp
+++ modules/tracking/main.cpp
@@ -3,22 +3,42 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgcodecs.hpp"
 
+
 using namespace cv;
 
 int main() {
     // open the first webcam plugged in the computer
     cv::VideoCapture camera(0);
+//    camera.set(cv::CAP_PROP_FRAME_HEIGHT, 500);
+//    //camera.set(cv::CAP_PROP_FRAME_WIDTH, 500);
+    camera.set(cv::CAP_PROP_FPS, 30);
+
+
+
     if (!camera.isOpened()) {
         std::cerr << "ERROR: Could not open camera" << std::endl;
         return 1;
     }
 
+
+
+
     // create a window to display the images from the webcam
-    cv::namedWindow("Webcam", WINDOW_AUTOSIZE);
+    cv::namedWindow("Webcam", WINDOW_NORMAL| WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED);
 
     // this will contain the image from the webcam
     cv::Mat frame;
-
+    cv::Mat greyframe;
+    struct location{
+        double minX;
+        double minY;
+        Point min1;
+        Point max1;
+    };
+    auto* Location = new location();
+    std::vector<std::vector<cv::Point>> circles;
+    std::vector<cv::RotatedRect> ellipses;
+    cv::Mat smallframe;
     // capture the next frame from the webcam
 
 
@@ -26,9 +46,34 @@ int main() {
     while (1) {
         camera >> frame;
         // show the image on the window
-        cv::imshow("Webcam", frame);
+        ellipses.clear();
+        cv::cvtColor(frame, greyframe, cv::COLOR_BGR2GRAY);
+//        // convert to grey
+//        cv::minMaxLoc(greyframe, &Location->minX, &Location->minY, &Location->min1, &Location->max1);
+//        cv::circle(frame, Location->max1, 10, cv::Scalar(255, 0, 0), 4);
+        cv::threshold(greyframe, greyframe, 200, 255, cv::THRESH_BINARY);
+        cv::findContours(greyframe, circles, cv::RETR_LIST, CHAIN_APPROX_NONE);
+
+        for(int idx = 0; idx < circles.size(); idx++){
+            std::vector<cv::Point> _smallC = circles.at(idx);
+            if(_smallC.size() > 100){
+                ellipses.push_back(cv::fitEllipse(_smallC));
+            }
+
+        }
+
+
+        for(int idx = 0; idx < circles.size(); idx++){
+            cv::drawContours(frame, circles, idx, Scalar(200, 200, 1), 1);
+        }
+        for(auto& rect : ellipses){
+            cv::ellipse(frame, rect, Scalar(0, 255, 0), 5);
+        }
+
+        cv::resize(frame, smallframe, Size(), 1, 1);
+        cv::imshow("Webcam", smallframe);
         // wait (10ms) for a key to be pressed
-        sleep(0.1);
+
         if (cv::pollKey() != -1) break;
     }
     return 0;
